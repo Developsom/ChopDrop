b854acc003d9e481bdcfd319838c898fa6c59f0b
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-23 19:34:11 +0200

    Setup pseudo code

--- src/ChopDrop.java
+++ src/ChopDrop.java
@@ -1,11 +1,42 @@
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
+import java.awt.*;
+
 @ScriptManifest(author = "BackToRS", info = "Chop and Drop any specified tree(s)", logo = "", name = "Chop Drop", version = 1)
 public class ChopDrop extends Script {
     @Override
-    public int onLoop() throws InterruptedException {
-        return 0;
+
+    public void onStart() {
+
+        //Code here will execute before the loop is started
+
     }
+
+
+
+    @Override
+
+    public void onExit() {
+
+        //Code here will execute after the script ends
+
+    }
+
+    @Override
+
+    public int onLoop() {
+
+        return 100; //The amount of time in milliseconds before the loop starts over
+
+    }
+
+    @Override
+
+    public void onPaint(Graphics2D g) {
+
+        //This is where you will put your code for paint(s)
+
+    }
+
 }
-hgg
