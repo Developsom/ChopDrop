caadb9f2844969fbf8227ada72db6db702cb9d17
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-23 21:27:59 +0200

    Added trees and logs

--- src/ChopDrop.java
+++ src/ChopDrop.java
@@ -1,20 +1,25 @@
-import org.osbot.rs07.api.map.Position;
+import org.osbot.rs07.api.Inventory;
 import org.osbot.rs07.api.model.RS2Object;
-import org.osbot.rs07.script.MethodProvider;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
-@ScriptManifest(name = "ChopDrop", author = "BackToRS", version = 1.0, info = "Chop and drop", logo = "")
-public class ChopDrop extends Script {
-    private final int TREE_ID = 12345; // Replace with the actual tree object ID
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-    private final Position[] treePositions = {
-            new Position(x1, y1, z1), // Add the positions of the trees you want to chop
-            new Position(x2, y2, z2),
-            // Add more positions as needed
-    };
+@ScriptManifest(name = "Woodcutter", author = "Your Name", version = 1.0, info = "", logo = "")
+public class WoodcutterScript extends Script implements ActionListener {
+    private final List<String> treeOptions = new ArrayList<>();
+    private final List<String> logOptions = new ArrayList<>();
+    private String selectedTree;
+    private String selectedLog;
 
-    private boolean dropLogs = false;
+    @Override
+    public void onStart() {
+        setupGUI();
+    }
 
     @Override
     public int onLoop() throws InterruptedException {
@@ -29,23 +34,72 @@ public class ChopDrop extends Script {
     }
 
     private void chopTree() {
-        RS2Object tree = getObjects().closest(TREE_ID);
+        RS2Object tree = getObjects().closest(object -> object != null && object.getName().equals(selectedTree));
         if (tree != null && tree.isVisible()) {
             if (tree.interact("Chop down")) {
                 MethodProvider.sleepUntil(() -> myPlayer().isAnimating(), random(3000, 5000));
             }
         } else {
             // Walk to the nearest tree
-            getWalking().webWalk(treePositions);
+            getWalking().webWalk(tree.getPosition());
         }
     }
 
     private void dropLogs() {
-        getInventory().dropAll(log -> log != null && log.getName().toLowerCase().contains("log"));
+        Inventory inventory = getInventory();
+        inventory.dropAll(item -> item != null && item.getName().equals(selectedLog));
+    }
+
+    private void setupGUI() {
+        treeOptions.add("Tree");
+        treeOptions.add("Oak");
+        treeOptions.add("Willow");
+        treeOptions.add("Teak");
+
+        logOptions.add("Logs");
+        logOptions.add("Oak logs");
+        logOptions.add("Willow logs");
+        logOptions.add("Teak logs");
+
+        selectedTree = treeOptions.get(0);
+        selectedLog = logOptions.get(0);
+
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("Woodcutter");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 150);
+            frame.setLayout(new FlowLayout());
+
+            JComboBox<String> treeSelector = new JComboBox<>(treeOptions.toArray(new String[0]));
+            treeSelector.addActionListener(e -> selectedTree = (String) treeSelector.getSelectedItem());
+
+            JComboBox<String> logSelector = new JComboBox<>(logOptions.toArray(new String[0]));
+            logSelector.addActionListener(e -> selectedLog = (String) logSelector.getSelectedItem());
+
+            JButton startButton = new JButton("Start");
+            startButton.addActionListener(this);
+
+            frame.add(treeSelector);
+            frame.add(logSelector);
+            frame.add(startButton);
+            frame.setVisible(true);
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Start")) {
+            log("Woodcutter script started!");
+            EventQueue.invokeLater(() -> {
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor((Component) e.getSource());
+                frame.dispose();
+            });
+        }
     }
 
     @Override
     public void onExit() {
-        log("Thank you for using ChopDrop!");
+        log("Thank you for using WoodcutterScript!");
     }
 }
+
