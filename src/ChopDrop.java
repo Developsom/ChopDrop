9a95ae32817384b85cffb5fc1b2ea4a3962e3b52
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-23 19:25:51 +0200

    Repo

--- src/ChopDrop.java
+++ src/ChopDrop.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ChopDrop {
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(author = "BackToRS", info = "Chop and Drop any specified tree(s)", logo = "", name = "Chop Drop", version = 1)
+public class ChopDrop extends Script {
+    @Override
+    public int onLoop() throws InterruptedException {
+        return 0;
+    }
 }
